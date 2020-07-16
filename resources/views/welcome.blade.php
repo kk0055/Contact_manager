fa4f5111c6bf1e694bfd9d2abb307c8a40ef02a3
kk0055 <62654316+kk0055@users.noreply.github.com>
2020-07-16 20:22:19 +0800

    Add files via upload

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+<meta name="csrf-token" content="{{csrf_token()}}">
+    <title>ContactStore</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+</head>
+<body>
+    <div class="container">
+    
+        <div id="app">
+            <contacts></contacts>
+        </div>
+    </div>
+    <script src="/js/app.js"></script>
+</body>
+</html>
